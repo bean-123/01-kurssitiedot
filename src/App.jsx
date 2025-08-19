1b1536d748254721aeb54ab969245bdc1a27b4f3
bean-123 <amy.platt@hotmail.com>
2025-08-19 13:05:18 +0300

    Adding info to components on task kurssitiedot

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,36 @@ const Header = (props) => {
   console.log(props);
   return (
     <>
-      <h2>{props.course}</h2>
+      <h2>komponentti Header</h2>
+      <p>- {props.course}</p>
+    </>
+  );
+};
+
+const Content = (props) => {
+  console.log(props);
+  return (
+    <>
+      <h2>komponentti Content</h2>
+      <p>
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
+      </p>
+    </>
+  );
+};
+
+const Total = (props) => {
+  console.log(props);
+  return (
+    <>
+      <h2>Komponentti total</h2>
+      <p>- Total: {props.total} exercises</p>
     </>
   );
 };
@@ -18,7 +47,7 @@ const App = () => {
 
   return (
     <>
-      <h1>Sovellus yhdessä komponentissa</h1>
+      <h1>1. Sovellus yhdessä komponentissa</h1>
       <h2>{course}</h2>
       <p>
         {part1} {exercises1}
@@ -32,8 +61,17 @@ const App = () => {
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
 
       <hr />
-      <h1>Sovellus purettuna komonentteihin Heading, Content ja Total</h1>
+      <h1>2. Sovellus purettuna komonentteihin Heading, Content ja Total</h1>
       <Header course={course} />
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
+      <Total total={exercises1 + exercises2 + exercises3} />
     </>
   );
 };
