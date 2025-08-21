7316fdbbce7704dc503bb3d029b84f2eaa741231
bean-123 <amy.platt@hotmail.com>
2025-08-21 13:00:50 +0300

    Refactor App to use course object and parts array

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ const Header = (props) => {
   return (
     <>
       <h2>komponentti Header</h2>
-      <p>- {props.course}</p>
+      <p>- {props.course.name}</p>
     </>
   );
 };
@@ -21,10 +21,14 @@ const Part = (props) => {
 
 const Total = (props) => {
   console.log(props);
+  const total =
+    props.parts[0].exercises +
+    props.parts[1].exercises +
+    props.parts[2].exercises;
   return (
     <>
       <h2>Komponentti total</h2>
-      <p>- Total: {props.total} exercises</p>
+      <p>- Total: {total} exercises</p>
     </>
   );
 };
@@ -34,49 +38,41 @@ const Content = (props) => {
   return (
     <>
       <h2>komponentti Content</h2>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
+      <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
     </>
   );
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
-
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      { name: "Fundamentals of React", exercises: 10 },
+      { name: "Using props to pass data", exercises: 7 },
+      { name: "State of a component", exercises: 14 },
+    ],
+  };
   return (
     <>
       <h1>1. Sovellus yhdessä komponentissa</h1>
-      <h2>{course}</h2>
+      <h2>{course.name}</h2>
+      <p>{course.parts[0].name}</p>
+      <p>{course.parts[1].name}</p>
+      <p>{course.parts[2].name}</p>
       <p>
-        {part1} {exercises1}
+        Number of exercises{" "}
+        {course.parts[0].exercises +
+          course.parts[1].exercises +
+          course.parts[2].exercises}
       </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
 
       <hr />
       <h1>2. Sovellus purettuna komonentteihin Heading, Content ja Total</h1>
       <Header course={course} />
-      <Content
-        part1={part1}
-        exercises1={exercises1}
-        part2={part2}
-        exercises2={exercises2}
-        part3={part3}
-        exercises3={exercises3}
-      />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </>
   );
 };
