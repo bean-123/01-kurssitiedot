1125848c4e30f39772752423be4de963bc8f4d2d
bean-123 <amy.platt@hotmail.com>
2025-09-16 11:59:45 +0300

    Added task 2.1-2.5 from fullstackopen

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,49 @@
-const Header = (props) => {
-  console.log(props);
+const Header = ({ course }) => {
+  console.log(course);
   return (
     <>
       <h2>komponentti Header</h2>
-      <p>- {props.course.name}</p>
+      <p>- {course.name}</p>
     </>
   );
 };
 
-const Part = (props) => {
-  console.log(props);
+const Part = ({ part }) => {
+  console.log(part);
   return (
     <>
       <p>
-        {props.part} {props.exercises}
+        {part.name} {part.exercises}
       </p>
     </>
   );
 };
 
-const Total = (props) => {
-  console.log(props);
-  const total =
-    props.parts[0].exercises +
-    props.parts[1].exercises +
-    props.parts[2].exercises;
+const Content = ({ parts }) => {
+  console.log(parts);
   return (
     <>
-      <h2>Komponentti total</h2>
-      <p>- Total: {total} exercises</p>
+      <h2>komponentti Content</h2>
+      {parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
     </>
   );
 };
 
-const Content = (props) => {
-  console.log(props);
+const Total = ({ parts }) => {
+  console.log(parts);
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
   return (
     <>
-      <h2>komponentti Content</h2>
-      <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
-      <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
-      <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
+      <h2>Komponentti total</h2>
+      <p>- Total of {total} exercises</p>
     </>
   );
 };
 
 const Course = ({ course }) => {
+  console.log(course);
   return (
     <div>
       <Header course={course} />
@@ -56,37 +54,50 @@ const Course = ({ course }) => {
 };
 
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    id: 1,
-    parts: [
-      { name: "Fundamentals of React", exercises: 10, id: 1 },
-      { name: "Using props to pass data", exercises: 7, id: 2 },
-      { name: "State of a component", exercises: 14, id: 3 },
-    ],
-  };
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        { name: "Fundamentals of React", exercises: 10, id: 1 },
+        { name: "Using props to pass data", exercises: 7, id: 2 },
+        { name: "State of a component", exercises: 14, id: 3 },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        { name: "Routing", exercises: 3, id: 1 },
+        { name: "Middlewares", exercises: 7, id: 2 },
+      ],
+    },
+  ];
+
   return (
     <>
       <h1>1. Sovellus yhdessä komponentissa</h1>
-      <h2>{course.name}</h2>
-      <p>{course.parts[0].name}</p>
-      <p>{course.parts[1].name}</p>
-      <p>{course.parts[2].name}</p>
+      <h2>{courses[0].name}</h2>
+      <p>{courses[0].parts[0].name}</p>
+      <p>{courses[0].parts[1].name}</p>
+      <p>{courses[0].parts[2].name}</p>
       <p>
         Number of exercises{" "}
-        {course.parts[0].exercises +
-          course.parts[1].exercises +
-          course.parts[2].exercises}
+        {courses[0].parts[0].exercises +
+          courses[0].parts[1].exercises +
+          courses[0].parts[2].exercises}
       </p>
 
       <hr />
       <h1>2. Sovellus purettuna komponentteihin Heading, Content ja Total</h1>
-      <Header course={course} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Header course={courses[0]} />
+      <Content parts={courses[0].parts} />
+      <Total parts={courses[0].parts} />
 
-      <h1>3. Step 6</h1>
-      <Course course={course} />
+      <h1> Step 2.5 </h1>
+      {courses.map((course) => (
+        <Course key={course.id} course={course} />
+      ))}
     </>
   );
 };
